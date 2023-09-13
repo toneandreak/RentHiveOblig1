daf80cedef7f1d8efc4e7a349bd731b11efa1348
toneandreak <89570275+toneandreak@users.noreply.github.com>
2023-09-13 13:45:43 +0200

    oppdatert database

--- RentHiveOblig/Models/Eiendom.cs
+++ RentHiveOblig/Models/Eiendom.cs
@@ -2,7 +2,7 @@
 {
     public class Eiendom
     {
-        public int Id { get; set; }
+        public int EiendomId { get; set; }
         public string EiendomName { get; set; }
         public string EiendomDescription { get; set; }
         public Eiendom() { }
