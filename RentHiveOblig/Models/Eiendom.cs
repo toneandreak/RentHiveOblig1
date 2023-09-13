db63b1e66f2e6e302e3c8ae4398d3eb9f756f486
toneandreak <89570275+toneandreak@users.noreply.github.com>
2023-09-13 13:49:49 +0200

    lagt til fra forrige fil i eiendom

--- RentHiveOblig/Models/Eiendom.cs
+++ RentHiveOblig/Models/Eiendom.cs
@@ -2,9 +2,21 @@
 {
     public class Eiendom
     {
-        public int EiendomId { get; set; }
+        public int EiendomID { get; set; }
         public string EiendomName { get; set; }
         public string EiendomDescription { get; set; }
+
+        public int Pris { get; set; }
+        public string Tittel { get; set; }
+        public string Beskrivelse { get; set; }
+        public string Sted { get; set; }
+        public int Soverom { get; set; }
+        public int Bad { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
         public Eiendom() { }
    
     }
