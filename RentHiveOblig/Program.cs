f9fe5ec3d732ebb1e0ed0ace3d2fd29ea24804dc
AdrianAnadolli <89570169+AdrianAnadolli@users.noreply.github.com>
2023-09-15 10:47:02 +0200

    Added dynamic carousel and client side files.

--- RentHiveOblig/Program.cs
+++ RentHiveOblig/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
