0f3ee28f1c9e4c4d5a214520e924bf4fa8b665e7
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-09-18 18:06:56 +0200

    Changes to the "Eiendom" model. Specifying required/optional attributes and ErrorMessages.

--- RentHiveOblig/Models/Eiendom.cs
+++ RentHiveOblig/Models/Eiendom.cs
@@ -1,22 +1,28 @@
-﻿namespace RentHiveOblig.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentHiveOblig.Models
 {
     public class Eiendom
     {
         public int EiendomID { get; set; }
-        public string EiendomName { get; set; }
-        public string EiendomDescription { get; set; }
 
-        public int Pris { get; set; }
+        [Required(ErrorMessage = "Required to fill out the price of the listing")]
+        public double PrisPerNatt { get; set; }
+
+        [Required(ErrorMessage = "Required to fill out Title for the listing")]
         public string Tittel { get; set; }
-        public string Beskrivelse { get; set; }
+        public string? Beskrivelse { get; set; }
         public string Sted { get; set; }
+
+        [Required(ErrorMessage = "Required to fill out how many bedrooms")]
         public int Soverom { get; set; }
+        [Required(ErrorMessage ="Required to fill out how many bathrooms")]
         public int Bad { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
-        public string Image1 { get; set; }
-        public string Image2 { get; set; }
-        public string Image3 { get; set; }
+        public string? Image1 { get; set; } //Optional images.
+        public string? Image2 { get; set; } //Optional images.
+        public string? Image3 { get; set; } //Optional images.
 
 
         public ICollection<Review> Reviews { get; set; }  //Allows Entity Frameowrk load related reviews. 
