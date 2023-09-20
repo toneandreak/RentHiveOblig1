cdc8e1f7f51fd525fd331192e710e058d4ed9057
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-09-20 13:56:59 +0200

    ++

--- RentHiveOblig/Models/Message.cs
+++ RentHiveOblig/Models/Message.cs
@@ -1,6 +1,34 @@
-﻿namespace RentHiveOblig.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentHiveOblig.Models
 {
     public class Message
     {
+        [Key]
+        public int MessageId { get; set; }
+
+        [ForeignKey("Conversation")]
+        public int ConversationId { get; set; }
+
+        // Navigation property for the conversation
+        public Conversation Conversation { get; set; }
+
+        // Foreign key for the sender
+        [ForeignKey("Sender")]
+        public int SenderId { get; set; } //Needed to identify who the sender of the message is. Does not need a Receiver here because it is determined by the Conversation. 
+
+        // Navigation property for the sender
+        public Bruker Sender { get; set; }
+
+
+
+        // The text of the message.
+        [MaxLength(500, ErrorMessage = "Content cannot exceed 500 characters.")]
+        [Required(ErrorMessage = "Content is required to send a message.")]
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.Now;  // Sets the default timestamp. 
     }
 }
+
