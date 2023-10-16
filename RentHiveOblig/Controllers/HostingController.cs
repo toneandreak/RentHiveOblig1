32852bf3dfb979d87d47924d0a3224d1df4a847e
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-10-16 16:39:02 +0200

    Updated the HostingController to get only listings for that user.

--- RentHiveOblig/Controllers/HostingController.cs
+++ RentHiveOblig/Controllers/HostingController.cs
@@ -1,16 +1,36 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentHiveOblig.Data;
+using RentHiveOblig.Data.Migrations;
+using SQLitePCL;
+using System.Security.Claims;
 
 namespace RentHiveOblig.Controllers
 {
 
 
+
     [Authorize]
     public class HostingController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+
+        public HostingController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+
+        public async Task <IActionResult> Index()
+        {
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var userEiendoms = await _context.Eiendom.Where(e => e.ApplicationUserId == userId).ToListAsync();
+            return View(userEiendoms);
         }
     }
 }
